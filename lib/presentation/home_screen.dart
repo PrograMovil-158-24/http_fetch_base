ab2eef11b27d80175c5ecb46cd7005c9e4f20079
Ivan Cruz <ivancruzhn@gmail.com>
2024-02-21 20:08:21 -0600

    statefull widget display data

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_http_fetch/core/requests/album_requests.dart';
+import 'package:flutter_http_fetch/domain/album.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:const Text('Home Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-          },
-          child:const Text('Go to Detail Screen'),
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
         ),
-      ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: fetchAlbum(), // Add parentheses here
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.title);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        )
     );
   }
 }
